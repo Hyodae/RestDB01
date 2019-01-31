9d9c5c480cc76ba578eb57ad0f3b2f4245105da2
Hyodae <hyodaekim@gmail.com>
2019-01-31 14:30:17 +0900

    First commit

--- src/java/com/hyodae/restdb/REST_Endpoint.java
+++ src/java/com/hyodae/restdb/REST_Endpoint.java
@@ -40,7 +40,6 @@ public class REST_Endpoint {
      * @return an instance of java.lang.String
      */
     @GET
-//    @Produces(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_HTML)
     public String getText() {
         
@@ -69,83 +68,6 @@ public class REST_Endpoint {
         }        
         
         return resultPage;
-       
-/*        
-        String driver = "com.mysql.jdbc.Driver";
-        String url = "jdbc:mysql://emba.kr:3306/sample";
-        String sql = "select * from customer";
-        
-        Connection con = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        
-        String result = "";
- 
-        try {
-            Class.forName(driver);
-            con = DriverManager.getConnection(url, "root", "wjfjs202");
-            System.out.println("Success to connect DB");
-
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(sql);
-
-            result = 
-                    "<!DOCTYPE html PUBLIC>\n" +
-                    "<html>\n" +
-                    "<head>\n" +
-                    "<title>Table</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "    <table border=\"1\">\n" +
-                    "       <th>Customer ID</th>\n" +
-                    "       <th>Discount Code</th>\n" +
-                    "       <th>Zip</th>\n" +
-                    "       <th>Name</th>\n" +
-                    "       <th>Address1</th>\n" +
-                    "       <th>Address2</th>\n" +
-                    "       <th>City</th>\n" +
-                    "       <th>State</th>\n" +
-                    "       <th>Phone</th>\n" +
-                    "       <th>Fax</th>\n" +
-                    "       <th>Email</th>\n" +
-                    "       <th>Credit Limit</th>\n";
-            while (rs.next()) {
-                result = result
-                        + "<tr>"
-                        + "<td>" + rs.getInt("customer_id") + "</td>"
-                        + "<td>" + rs.getString("discount_code") + "</td>"
-                        + "<td>" + rs.getString("zip") + "</td>"
-                        + "<td>" + rs.getString("name") + "</td>"
-                        + "<td>" + rs.getString("addressline1") + "</td>"
-                        + "<td>" + rs.getString("addressline2") + "</td>"
-                        + "<td>" + rs.getString("city") + "</td>"
-                        + "<td>" + rs.getString("state") + "</td>"
-                        + "<td>" + rs.getString("phone") + "</td>"
-                        + "<td>" + rs.getString("fax") + "</td>"
-                        + "<td>" + rs.getString("email") + "</td>"
-                        + "<td>" + rs.getInt("credit_limit") + "</td>"
-                        + "</tr>";
-            }
-
-            result = result +
-                    "</table>\n" +
-                    "</body>\n" +
-                    "</html>";
-        }
-        catch(Exception e) {
-                System.out.println("Failed to connect DB");
-        }
-        finally {
-                try {
-                        if (con!=null) con.close();
-                }
-                catch (Exception e) {
-                        System.out.println(e.getMessage());
-                }
-        }        
-        
-        return result;
-*/        
     }
 
     /**
